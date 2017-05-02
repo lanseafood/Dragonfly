62e4e73e0ed1541e07caa29e81aa8894102a20d8
Lanssie Ma <l.mao.ftwin@gmail.com>
2017-05-02 12:56:31 -0700

    color swapping works

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -56,121 +56,15 @@ ros::Publisher meshClick_pub;
 // initializing the talker
 
 
-void processFeedback(
-    const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
-{
-  std::ostringstream s;
-  s  << feedback->marker_name << "' "
-      << "control '" << feedback->control_name << "'";
-
-  std::ostringstream mouse_point_ss;
-  if( feedback->mouse_point_valid )
-  {
-    mouse_point_ss << " at " << feedback->mouse_point.x
-                   << ", " << feedback->mouse_point.y
-                   << ", " << feedback->mouse_point.z
-                   << " in frame " << feedback->header.frame_id;
-    meshClick_pub.publish(feedback);             
-  }
-
-  server->applyChanges();
-}
-
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "meshClickTalker");
+  ros::init(argc, argv, "fakePoses");
 
   ros::NodeHandle n;
 
-  meshClick_pub = n.advertise<std_msgs::String>("meshClick_chatter", 1000);
+  meshClick_pub = n.advertise<std_msgs::String>("fakePoses_chatter", 1000);
   
-  // ros::Rate loop_rate(10);
-
-  ros::init(argc, argv, "pub_marker");
-
-  server.reset( new interactive_markers::InteractiveMarkerServer("basic_controls","",false) );
-  // server1.reset( new interactive_markers::InteractiveMarkerServer("basic_controls","",false) );
-
-  menu_handler_finger.insert( "finger Entry", &processFeedback );
-  menu_handler_mount.insert( "mount Entry", &processFeedback );
-  
-  // create an interactive marker server on the topic namespace simple_marker
-  interactive_markers::InteractiveMarkerServer server("pub_marker");
-  // interactive_markers::InteractiveMarkerServer server("tester_marker");
-
-  // create an interactive marker for our server
-  visualization_msgs::InteractiveMarker finger_int_marker;
-  finger_int_marker.header.frame_id = "base";
-  finger_int_marker.header.stamp=ros::Time::now();
-  finger_int_marker.name = "finger_marker_test";
-  finger_int_marker.description = "Testing_finger_marker";
-  // //tester
-  visualization_msgs::InteractiveMarker int_marker_mount;
-  int_marker_mount.header.frame_id = "mount";
-  int_marker_mount.header.stamp=ros::Time::now();
-  int_marker_mount.name = "mount_menu_test";
-  int_marker_mount.description = "Testing_mount_marker";
-
-  //interactive menu control;
-  InteractiveMarkerControl menu_control;
-  menu_control.interaction_mode = InteractiveMarkerControl::MENU;
-  menu_control.name = "menu_control";
-  
-  InteractiveMarkerControl menu_control_mount;
-  menu_control_mount.interaction_mode = InteractiveMarkerControl::MENU;
-  menu_control_mount.name = "menu_control_mount";
-
-
-  // create a grey finger marker
-  visualization_msgs::Marker finger;
-  finger.type = visualization_msgs::Marker::MESH_RESOURCE;
-  finger.mesh_resource = "package://rrbot_description/meshes/finger.stl";
-  // finger.type = visualization_msgs::Marker::CUBE;
-  finger.scale.x = .01;
-  finger.scale.y = .01;
-  finger.scale.z = .01;
-  finger.color.r = 0.5;
-  finger.color.g = 0.5;
-  finger.color.b = 0.5;
-  finger.color.a = 1.0;
-
-    // create a grey mount marker
-  visualization_msgs::Marker mount;
-  mount.type = visualization_msgs::Marker::MESH_RESOURCE;
-  mount.mesh_resource = "package://rrbot_description/meshes/mount.stl";
-  // mount.type = visualization_msgs::Marker::CUBE;
-  mount.scale.x = .01;
-  mount.scale.y = .01;
-  mount.scale.z = .01;
-  mount.color.r = 0.5;
-  mount.color.g = 0.5;
-  mount.color.b = 0.5;
-  mount.color.a = 1.0;
-
-
-  // ADD MARKERS TO CONTROLS 
-  menu_control.markers.push_back(finger);
-  menu_control.always_visible = true;
-
-  menu_control_mount.markers.push_back(mount);
-  menu_control_mount.always_visible = true;
-
-  // ADD CONTROLS TO MARKERS 
-  finger_int_marker.controls.push_back( menu_control);
-  int_marker_mount.controls.push_back(menu_control_mount);
-
-  //add marker to int marker
-  // add the interactive marker to our collection &
-  // tell the server to call processFeedback() when feedback arrives for it
-  server.insert(finger_int_marker, &processFeedback);
-  server.insert(int_marker_mount, &processFeedback);
-  // 'commit' changes and send to all clients
-  menu_handler_finger.apply( server, finger_int_marker.name);
-  server.applyChanges();
-  
-  menu_handler_mount.apply( server, int_marker_mount.name);
-  // menu_handler_f2.apply( server, int_marker.name);
-  server.applyChanges();
+  //meshClick_pub.publish("test");    
 
 
   // start the ROS main loop
--- src/test_marker.cpp
+++ src/test_marker.cpp
@@ -1,11 +1,12 @@
 #include <ros/ros.h>
 
-#include <interactive_markers/interactive_marker_server.h>
-#include <interactive_markers/menu_handler.h>
+#include "../../../interactive_markers/include/interactive_markers/interactive_marker_server.h"
+#include "../../../interactive_markers/include/interactive_markers/menu_handler.h"
 
+#include <utility>
 #include <tf/transform_broadcaster.h>
 #include <tf/tf.h>
-
+#include <typeinfo>
 #include <math.h>
 
 #include "std_msgs/String.h"
@@ -15,29 +16,23 @@
 using namespace visualization_msgs;
 ros::Publisher meshClick_pub;
 
-// Clickable for markers
-void onClick(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback) {
-  ROS_INFO_STREAM("CONTROLLER " + feedback->marker_name + " CLICKED ");
-  std_msgs::String news;
-  std::ostringstream s;
-  s << feedback->marker_name;
-  news.data = s.str();
-  meshClick_pub.publish(news);
-}
-
+boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server;
+//map of ID's of markers and color to switch between when clicked
+std::map<std::string, std::pair<int, std::string> > markers_color_map;
 
 // Function to insert a marker with a specific name
-visualization_msgs::InteractiveMarker InsertMarker(std::string const& name, float x, float y, std::string const stl) {
+const visualization_msgs::InteractiveMarker MakeMarker(std::string const& name, tf::Vector3& position, float color, std::string stl, std::string des) {
   // Create the interactive marker
   visualization_msgs::InteractiveMarker int_marker;
   int_marker.header.frame_id = "base_link";
   int_marker.header.stamp = ros::Time::now();
+  tf::pointTFToMsg(position, int_marker.pose.position);
   int_marker.name = name;
-  int_marker.description = "Simple button Control";
+  int_marker.description = des;
   // int_marker.pose.position.x = x;
   // int_marker.pose.position.y = y;
   int_marker.scale = 1.0;
-
+ 
 //Create a mesh marker
   visualization_msgs::Marker mesh_marker;
   mesh_marker.type = visualization_msgs::Marker::MESH_RESOURCE;
@@ -45,7 +40,7 @@ visualization_msgs::InteractiveMarker InsertMarker(std::string const& name, floa
   mesh_marker.scale.x = .01;
   mesh_marker.scale.y = .01;
   mesh_marker.scale.z = .01;
-  mesh_marker.color.r = 0.5;
+  mesh_marker.color.r = color;
   mesh_marker.color.g = 0.5;
   mesh_marker.color.b = 0.5;
   mesh_marker.color.a = 1.0;
@@ -64,21 +59,105 @@ visualization_msgs::InteractiveMarker InsertMarker(std::string const& name, floa
   return int_marker;
 }
 
+// Function to insert a marker with a specific name
+void updateMarker(visualization_msgs::InteractiveMarker int_marker) {
+  server->insert(int_marker);
+} 
+
+// Clickable for markers
+void onClick(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback) {
+
+  std::cout << *feedback << std::endl;
+  std::cout << feedback->pose.position << std::endl;
+  std::cout << typeid(feedback->pose.position).name() << std::endl;
+
+  //getting marker name
+  std::string name = feedback->marker_name;
+  std::cout << "name of marker " << name << std::endl;
+  //outputting to cout
+  ROS_INFO_STREAM("CONTROLLER " + name + " CLICKED ");
+
+  //publishing to newsfeed
+  std_msgs::String news;
+  std::ostringstream s;
+  s << name;
+  news.data = s.str();
+  meshClick_pub.publish(news);
+
+  //erasing old marker
+  server->erase(name);
+  std::cout << "erased old one" << std::endl;
+
+
+  //checking color to change to
+  float color;
+  std::string d;
+  int flip = markers_color_map[name].first;
+  if (flip == 0)
+  {
+    color = 255.0;
+    d = name;
+    markers_color_map[name].first = 1;
+
+  }
+  else
+  {
+    color = 0.5;
+    d = "";
+    markers_color_map[name].first = 0;
+  }
+
+  //geting new position
+  //tf::Vector3 position = feedback->pose.position;
+  tf::Vector3 position = tf::Vector3(0,0,0);
+  //getting stl
+  std::string stl = markers_color_map[name].second;
+  std::cout << name << position << color << stl << std::endl;
+  //replacing IM
+  visualization_msgs::InteractiveMarker int_marker = MakeMarker(name, position, color, stl, d);
+
+  updateMarker(int_marker);
+
+  server->applyChanges();
+
+}
+
+
 int main(int argc, char** argv) {
   // Initialize ROS
   ros::init(argc, argv, "clickable_markers_server");
   // create an interactive marker server on the topic namespace simple_marker
-  interactive_markers::InteractiveMarkerServer server("IM_example_marker");
-
   ros::NodeHandle n;
-
+  server = boost::shared_ptr<interactive_markers::InteractiveMarkerServer>(new interactive_markers::InteractiveMarkerServer ("IM_example_marker"));
+  
   meshClick_pub = n.advertise<std_msgs::String>("meshClick_chatter", 1000);
- 
-  // Use two different controller types
-  server.insert(InsertMarker("marker_finger", 0.0, -1.0, "package://rrbot_description/meshes/finger.stl"), &onClick);
-  server.insert(InsertMarker("marker_mount", 0.0,  1.0, "package://rrbot_description/meshes/mount.stl"), &onClick);
+
+  //get positions
+  tf::Vector3 position_finger = tf::Vector3(0, 0, 0);
+  tf::Vector3 position_mount = tf::Vector3(0, 0, 0);
+  
+  //make pairs
+  std::pair<int, std::string> finger_pair (0,"package://rrbot_description/meshes/finger.stl");
+  std::pair<int, std::string> mount_pair (0,"package://rrbot_description/meshes/mount.stl");
+
+  //make markers for each part
+  visualization_msgs::InteractiveMarker int_marker_1 = MakeMarker("marker_finger", position_finger, 0.5, finger_pair.second, "");
+  visualization_msgs::InteractiveMarker int_marker_2 = MakeMarker("marker_mount", position_mount, 0.5, mount_pair.second, "");
+
+    // Use two different controller types
+  server->insert(int_marker_1, &onClick);
+  server->insert(int_marker_2, &onClick);
+
+  //update Map
+  markers_color_map["marker_finger"] =  finger_pair;
+  markers_color_map["marker_mount"] =  mount_pair;
+
   // 'commit' changes and send to all clients
-  server.applyChanges();
+  server->applyChanges();
+
+  //std::cout<< server.marker_contexts_;
+
   // start the ROS main loop
+  // std::cout << "hi";
   ros::spin();
 }
